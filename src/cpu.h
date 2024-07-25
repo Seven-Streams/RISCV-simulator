428bb06a96b3690f6d5fcb82c44d219ee24f404f
Yuchuan <Blemiade_Qinchuan@sjtu.edu.cn>
2024-07-25 13:42:44 +0800

    feat:change the order of the instructions. Add the processing of a few instructions.

--- src/cpu.h
+++ src/cpu.h
@@ -1,5 +1,6 @@
 #include "alu.h"
 #include "decoder.h"
+#include "isa.h"
 #include "lsb.h"
 #include "memory.h"
 #include "rf.h"
@@ -22,9 +23,47 @@ struct CPU {
     if (now_pc == -1) {
       return std::pair<bool, unsigned char>(true, reg[0]);
     }
-    unsigned char code[4] = {memory.mem[now_pc], memory.mem[now_pc + 1],
-                             memory.mem[now_pc + 2], memory.mem[now_pc + 3]};
+    unsigned char code[4] = {
+        memory.mem[advanced_pc], memory.mem[advanced_pc + 1],
+        memory.mem[advanced_pc + 2], memory.mem[advanced_pc + 3]};
     RawInstruction instruct = decode(code);
+    if (!rob.input.busy) {
+      Buffer to_push;
+      to_push.type = instruct.type;
+      to_push.busy = true;
+      to_push.OK = false;
+      int num = rob.tail;
+      if (instruct.type == HALT) {
+        to_push.des = -1;
+        to_push.OK = true;
+        to_push.value = 0;
+        rob.input = to_push;
+      }
+      if (instruct.type == AUIPC) {
+        to_push.des = instruct.rd;
+        to_push.OK = true;
+        to_push.value = advanced_pc + instruct.imm;
+        rob.input = to_push;
+        advanced_pc += instruct.imm;
+        rf.dependency[to_push.des] = num;
+      }
+      if (instruct.type == LUI) {
+        to_push.des = instruct.rd;
+        to_push.OK = true;
+        to_push.value = instruct.imm;
+        rob.input = to_push;
+        advanced_pc += 4;
+        rf.dependency[to_push.des] = num;
+      }
+      if(instruct.type == JAL) {
+        to_push.des = instruct.rd;
+        to_push.OK = true;
+        to_push.value = instruct.imm;
+        rob.input = to_push;
+        advanced_pc += instruct.imm;
+        rf.dependency[to_push.des] = num;
+      }
+    }
     return std::pair<bool, unsigned char>(false, 0);
   }
 };
--- src/isa.h
+++ src/isa.h
@@ -2,34 +2,14 @@
 #define ISA_H
 namespace Yuchuan {
 enum opcode {
-  ADD,
-  AND,
-  OR,
-  SLL,
-  SRL,
-  SLT,
-  SLTU,
-  SRA,
-  SUB,
-  XOR,
+  SB,
+  SH,
+  SW, // S type, 2
   LB,
   LBU,
   LH,
   LHU,
   LW,
-  SB,
-  SH,
-  SW,
-  AUIPC,//This instruction doesn't need to emit to somewhere else.
-  BEQ,
-  BGE,
-  BLT,
-  BNE,
-  BGEU,
-  BLTU,
-  JAL,//This instruction doesn't need to emit to somewhere else.
-  JALR,
-  LUI,
   ADDI,
   SLTI,
   SLTIU,
@@ -39,6 +19,26 @@ enum opcode {
   SLLI,
   SRLI,
   SRAI,
+  JALR, // I type, 17
+  ADD,
+  AND,
+  OR,
+  SLL,
+  SRL,
+  SLT,
+  SLTU,
+  SRA,
+  SUB,
+  XOR, // R type, 27
+  BEQ,
+  BGE,
+  BLT,
+  BNE,
+  BGEU,
+  BLTU, // B type, 33
+  JAL, // This instruction doesn't need to emit to somewhere else. J type, 34.
+  LUI,
+  AUIPC, // This instruction doesn't need to emit to somewhere else. U type, 36.
   HALT
 };
 }
