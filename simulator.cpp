487b459936555c9f8675a5c62a282bc66a5c8c92
Yuchuan <Blemiade_Qinchuan@sjtu.edu.cn>
2024-07-26 09:45:07 +0800

    feat:add the CMakeLists.

--- simulator.cpp
+++ simulator.cpp
@@ -3,7 +3,9 @@
 #include <iostream>
 #include <string>
 int main() {
-  freopen("sample.data", "r", stdin);
+  std::string name;
+  std::cin >> name;
+  freopen(name.c_str(), "r", stdin);
   Yuchuan::CPU barris;
   std::string res;
   unsigned int addr;
